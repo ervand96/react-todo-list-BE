473e242cf3086316f293d4117da13faf27ebb0d6
ervand96 <ervandharutyunyan06096@gmail.com>
2023-02-24 19:15:26 +0400

    add project

---
+++ api/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+
+const PORT = process.env.PORT ?? 4000;
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.listen(PORT, () => {
+    console.log(`Server ${PORT}`);
+})
